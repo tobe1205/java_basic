14fb3930bba455a22d987d8dafa6b942d0d133cb
tobe1205@hanmail.net <tobe1205@hanmail.net>
2021-11-30 13:49:16 +0900

    [21.11.30] # 초기 설정 커밋

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("안녕안녕");
-
+        System.out.println("오옹~~");
         System.out.println("메롱메롱~~");
         System.out.println("하이하이~~");
     }
