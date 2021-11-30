8aee91256c18c5877575f1af9d30f14a2032ce93
tobe1205 <tobe1205@hanmail.net>
2021-11-30 11:34:51 +0900

    [21.11.30] # 초기 설정 커밋

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+import sun.awt.geom.AreaOp;
+
+import javax.sound.midi.Soundbank;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("안녕안녕");
+    }
+}
