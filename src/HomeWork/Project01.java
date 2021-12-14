e881f786ede705a103119be82807d8715affc4d3
tobe1205 <dydrkfl7949@gmail.com>
2021-12-14 17:18:16 +0900

    [21.12.14] 자바 기초 - 팀프로젝트

--- src/HomeWork/Project01.java
+++ src/HomeWork/Project01.java
@@ -6,9 +6,13 @@ import java.util.Scanner;
 public class Project01 {
 
     //출근자 이름배열
-    static String[] empNames = {};
+    static String[] empNames = {"kgw", "aaa", "bbb"};
+    static String[] empNumbers = {"0000", "0001", "0002"};
+    static String[] empPosition = {"사원", "대리", "과장"};
+
+    /*static String[] empNames = {};
     static String[] empNumbers = {};
-    static String[] empPosition = {};
+    static String[] empPosition = {};*/
 
     // 메뉴화면
     static void printMenu() {
@@ -32,6 +36,9 @@ public class Project01 {
 
     static int inputInt() {
         int n = sc.nextInt();
+        if (n > 7 || n <= 0) {
+            System.out.println("1 ~ 7사이로 입력해주세요.");
+        }
         sc.nextLine();
         return n;
     }
@@ -41,6 +48,57 @@ public class Project01 {
         return sc.nextLine();
     }
 
+    static void inputName(){
+        String name = inputString("이름: ");
+        String[] temp = new String[empNames.length + 1];
+        for (int i = 0; i < empNames.length; i++) {
+            temp[i] = empNames[i];
+        }
+        temp[temp.length - 1] = name;
+        empNames = temp;
+        temp = null;
+    }
+    static void inputNum() {
+        while (true) {
+            String num = inputString("사원 번호: ");
+            int index = -1;
+
+            for (int i = 0; i < empNumbers.length; i++) {
+                if (num.equals(empNumbers[i])) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1) {
+                if (num.length() > 4) {
+                    System.out.println("사원 번호는 4자리 숫자입니다.");
+                    System.out.println("다시 입력해주세요.");
+                } else {
+                    String[] temp2 = new String[empNumbers.length + 1];
+                    for (int i = 0; i < empNumbers.length; i++) {
+                        temp2[i] = empNumbers[i];
+                    }
+                    temp2[temp2.length - 1] = num;
+                    empNumbers = temp2;
+                    temp2 = null;
+                    break;
+                }
+            } else {
+                System.out.println("등록된 사원 번호입니다.");
+            }
+        }
+    }
+
+   static void inputPosition(){
+       String position = inputString("직책: ");
+       String[] temp3 = new String[empPosition.length + 1];
+       for (int i = 0; i < empPosition.length; i++) {
+           temp3[i] = empPosition[i];
+       }
+       temp3[temp3.length - 1] = position;
+       empPosition = temp3;
+       temp3 = null;
+    }
     public static void main(String[] args) {
 
         while (true) {
@@ -48,43 +106,34 @@ public class Project01 {
             int choice = inputInt();
 
             if (choice == 5) {
-                String name = inputString("이름: ");
-                String num = inputString("사원 번호: ");
-                String position = inputString("직책: ");
-
-                String[] temp = new String[empNames.length + 1];
-                String[] temp2 = new String[empNumbers.length + 1];
-                String[] temp3 = new String[empPosition.length + 1];
-
-                for (int i = 0; i < empNames.length; i++) {
-                    temp[i] = empNames[i];
-                }
-                temp[temp.length - 1] = name;
-                empNames = temp;
-                temp = null;
-
-                for (int i = 0; i < empNumbers.length; i++) {
-                    temp2[i] = empNumbers[i];
-                }
-                temp2[temp2.length - 1] = num;
-                empNumbers = temp2;
-                temp2 = null;
-
-                for (int i = 0; i < empPosition.length; i++) {
-                    temp3[i] = empPosition[i];
-                }
-                temp3[temp3.length - 1] = position;
-                empPosition = temp3;
-                temp3 = null;
+                inputName();
+                inputNum();
+                inputPosition();
+                System.out.println("등록이 완료되었습니다.");
             } else if (choice == 6) {
-                int index = -1;
-                System.out.println("------------ 사원 목록 ---------------");
+                if (empNames.length < 1) {
+                    System.out.println("* 등록된 사원이 없습니다.");
+                    System.out.println("* 사원 등록 후 조회해주시기 바랍니다.");
+                } else {
+                    System.out.println("------------ 사원 목록 ---------------");
+                    for (int i = 0; i < empNames.length; i++) {
+                        System.out.printf("이름 : %s\n", empNames[i]);
+                    }
+                }
 
+            } else if (choice == 7) {
+                System.out.println("프로그램을 종료하시겠습니까? [Y/N]");
+                String yesNo = sc.nextLine();
+                if (yesNo.equals("y") || yesNo.equals("Y")) {
+                    System.out.println("프로그램을 종료합니다.");
+                    System.exit(0);
+                }
             }
-            System.out.println("등록이 완료되었습니다.");
-            System.out.println(Arrays.toString(empNumbers));
             System.out.println(Arrays.toString(empNames));
+            System.out.println(Arrays.toString(empNumbers));
             System.out.println(Arrays.toString(empPosition));
-        }//main
-    }//class
-}
+        }//while
+
+
+    }//main
+}//class
