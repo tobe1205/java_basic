5122fdd1288123ef4fedfbf351adf2a43459a57c
tobe1205 <tobe1205@hanmail.net>
2021-12-23 12:43:40 +0900

    [21.12.23] # 객체와 클래스 - 객체배열 실습문제

---
+++ src/com/kh/hw/member/Test.java
@@ -0,0 +1,42 @@
+package com.kh.hw.member;
+
+import com.kh.hw.member.controller.MemberController;
+import com.kh.hw.member.model.vo.Member;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        MemberController mc = new MemberController();
+
+        //mc.insertMember("ggg", "고길동", "1234", "zzz", 'M', 33);
+
+//        Member[] members = mc.printAll();
+//        for (Member m : members) {
+//            if (m == null) break;
+//            System.out.println(m.inform());
+//        }
+
+//        Member[] members = mc.searchName("홍길동");
+//        for (Member m : members) {
+//            System.out.println(m.inform());
+//        }
+
+        /*mc.updatePassword("def", "999999!@");
+
+        Member member = mc.searchId("def");
+        System.out.println(member.inform());*/
+
+
+        mc.delete();
+
+
+
+        Member[] members = mc.printAll();
+        System.out.println(mc.existMemberNum());
+        for (Member m : members) {
+            if (m == null) break;
+            System.out.println(m.inform());
+        }
+    }
+}
