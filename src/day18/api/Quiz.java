2b2084e096ebba51c834547e614470a4f17898a6
tobe1205 <dydrkfl7949@gmail.com>
2022-01-03 13:05:32 +0900

    [21.12.31] 자바 기초 - Api 기초, 연습문제

--- src/day18/api/Quiz.java
+++ src/day18/api/Quiz.java
@@ -14,17 +14,21 @@ public class Quiz {
         System.out.printf("앞자리 : %s\n", num.substring(0, 6));
         System.out.printf("뒷자리 : %s\n", num.substring(7));
 
-        char genNum = num.charAt(7);
         int year = Integer.parseInt(num.substring(0, 2));
+        int month = Integer.parseInt(num.substring(2,4));
+        int day = Integer.parseInt(num.substring(4,6));
+
+        char genNum = num.charAt(7);
+
+        String gender = (genNum == '1' || genNum == '3') ? "남자" : "여자";
 
 
-        int birthYear = (genNum == '1' || genNum == '2') ?
-        } else if (genNum == '3' || genNum == '4')
-            year4 = 2000 + year2;
-        int year = LocalDateTime.now().getYear() - year4;
-        System.out.printf("%d년도 %d월 %d일생 %d세 %s", year, month, day, age, gender);
+        int birthYear = (genNum == '1'|| genNum == '2') ? 1900 + year : 2000 + year;
+        int age = LocalDateTime.now().getYear() - birthYear + 1;
 
+        System.out.printf("%d년도 %d월 %d일생 %d세 %s", birthYear, month, day, age, gender);
+
+        sc.close();
     }
-
 
 }
